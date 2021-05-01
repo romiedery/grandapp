1352bda00b624bfa71f1e19a3fe3a61f33be090d
Tomas Jalil <tomasjalil03@gmail.com>
2021-05-01 17:07:17 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/grandapp/MainActivity.java
+++ app/src/main/java/com/example/grandapp/MainActivity.java
@@ -18,6 +18,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     Button IrRegistro, IrLogin;
     @Override
@@ -26,11 +28,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         IrLogin=findViewById(R.id.IrLgn);
         IrRegistro=findViewById(R.id.irRgstr);
-        ConexionApi();
+        //ConexionApi();
     }
-public void ConexionApi(){
-URL miRuta= new URL("localhost:5000/API/");
-}
+//public void ConexionApi(){ URL miRuta= new URL("localhost:5000/API/");}
    public void irRegistro(View view){
        Intent intent = new Intent(this, Registro.class);
        startActivity(intent);
