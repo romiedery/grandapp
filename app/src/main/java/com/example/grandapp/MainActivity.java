0485d6877a85d7906bf3bd5e4d048c7980bc3c2f
Tomas Jalil <tomasjalil03@gmail.com>
2021-05-07 08:54:41 -0300

    Carga de API

--- app/src/main/java/com/example/grandapp/MainActivity.java
+++ app/src/main/java/com/example/grandapp/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,19 +20,48 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     Button IrRegistro, IrLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        IrLogin=findViewById(R.id.IrLgn);
-        IrRegistro=findViewById(R.id.irRgstr);
-        //ConexionApi();
+        IrLogin = findViewById(R.id.IrLgn);
+        IrRegistro = findViewById(R.id.irRgstr);
+        TareaAsincronica miTarea=new TareaAsincronica();
+        miTarea.execute();
     }
-//public void ConexionApi(){ URL miRuta= new URL("localhost:5000/API/");}
+
+        public class TareaAsincronica extends AsyncTask<Void,Void,Void> {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    URL miRuta = new URL(" http://epok.buenosaires.gob.ar/getCategorias%22");
+                            HttpURLConnection miConexion = (HttpURLConnection) miRuta.openConnection();
+                            Log.d("AccesoAPI","Me conecto");
+                    if (miConexion.getResponseCode() == 200) {
+
+                        Log.d("AccesoAPI", "todo perfecto");
+                        InputStream cuerpoRespuesta = miConexion.getInputStream();
+                        InputStreamReader respLeida = new InputStreamReader(cuerpoRespuesta, "UTF-8");
+                        }
+                    else {
+                        Log.d("AccesoAPI","Error en la conexion");
+                    }
+                    miConexion.disconnect();
+                } catch (Exception error) {
+                    Log.d("PresionoBoton", "no se creo una URL" + error.getMessage());
+                }
+                return null;
+            }
+        }
+
    public void irRegistro(View view){
        Intent intent = new Intent(this, Registro.class);
        startActivity(intent);
