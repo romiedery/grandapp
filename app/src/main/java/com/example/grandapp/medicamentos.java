7b0aea5b90d25bd895afa9b9a7942343579da4d9
Tomas Jalil <tomasjalil03@gmail.com>
2021-05-07 11:03:44 -0300

    Agregue 4 clases

---
+++ app/src/main/java/com/example/grandapp/medicamentos.java
@@ -0,0 +1,33 @@
+package com.example.grandapp;
+//MEDICAMENTOS
+
+
+public class medicamentos {
+    private int idMeds;
+    private String nombre;
+    private String tipo;
+
+    public int getidMeds() {
+        return idMeds;
+    }
+
+    public void setidMeds(int idMeds) {
+        idMeds = idMeds;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        nombre = nombre;
+    }
+
+    public String gettipo() {
+        return tipo;
+    }
+
+    public void settipo(String tipo) {
+        tipo = tipo;
+    }
+}
